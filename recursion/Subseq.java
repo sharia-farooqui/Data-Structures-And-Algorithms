ca8d185d5e3d91588695e1a0482de267df5de659
Sharia Farooqui <farooquisharia6@gmail.com>
2022-08-04 12:54:09 +0530

    update!

--- recursion/Subseq.java
+++ recursion/Subseq.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 
 public class Subseq{
     public static void main(String[] args) { 
-        System.out.println(subsets("abc", ""));
+        System.out.println(subseq("abc", ""));
     }
-    public static ArrayList<String> subsets(String str, String res){
+    public static ArrayList<String> subseq(String str, String res){
         if(str.isEmpty()){
             ArrayList<String> list = new ArrayList<String>();
             list.add(res);
             return list;
         }
         char ch = str.charAt(0);
-        ArrayList<String> left = subsets(str.substring(1), res+ch);
-        ArrayList<String> right = subsets(str.substring(1), res);
+        ArrayList<String> left = subseq(str.substring(1), res+ch);
+        ArrayList<String> right = subseq(str.substring(1), res);
         left.addAll(right);
         return left;
     }
--- recursion/Subsets.java
+++ recursion/Subsets.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Subsets {
-    public static void printSubsequence(int[] arr, int idx, ArrayList<Integer>list){
+    public static void printSubsets(int[] arr, int idx, ArrayList<Integer>list){
         if(idx==arr.length){
             if(list.size()>0){
                 System.out.println(list);
@@ -12,15 +12,15 @@ public class Subsets {
             return;
         }
         list.add(arr[idx]);
-        printSubsequence(arr, idx+1, list); 
+        printSubsets(arr, idx+1, list); 
         list.remove(list.size()-1);
-        printSubsequence(arr, idx+1, list);
+        printSubsets(arr, idx+1, list);
     }
     
     public static void main(String[] args) {
     int[] arr= {3,1,2};
     ArrayList<Integer> list = new ArrayList<>();
-    printSubsequence(arr, 0, list);
+    printSubsets(arr, 0, list);
     }
     
 }
