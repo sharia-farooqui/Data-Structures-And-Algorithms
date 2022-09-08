235e14931ff6f4a05f545ee7f2b3d3f7b0ef0195
Sharia Farooqui <farooquisharia6@gmail.com>
2022-09-08 14:19:13 +0530

    ll

--- linked_list/Main.java
+++ linked_list/Main.java
@@ -2,14 +2,17 @@ public class Main {
     public static void main(String[] args) {
         LL list = new LL();
         //whenever a new LL is created, size=0, head and tail will be initialised 
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list.insertLast(99);
+        // list.insertFirst(3);
+        // list.insertFirst(2);
+        // list.insertFirst(8);
+        // list.insertFirst(17);
+        // list.insertLast(99);
         // list.insertLastWTail(76);
-        list.insert(100, 3);
+        // list.insert(100, 3);
         // System.out.println(list.deleteLast());
+
+        list.inser
+
         list.delete(100);
         list.display();
     }
