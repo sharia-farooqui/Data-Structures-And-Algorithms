41f1ac2dc02a7bf586a4807b620836acef32bca8
sharia-farooqui <farooquisharia6@gmail.com>
2024-03-22 01:08:48 +0530

    UpdateIs

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -3,15 +3,15 @@ public class InsertionSort
 {
 	public static void main(String[] args) {
 		int[] arr =  {2,9,5,1,3};
-		for(int i=1;i<arr.length;i++){ 
-		    int val=arr[i];
-		    int j=i-1;
-		    while(j>=0&&arr[j]>val){
-		        arr[j+1]=arr[j];
-		        j--;
-		    }
-		    arr[j+1]=val;
-		}
+		for(int i=0;i<arr.length;i++){
+	        int j=i;
+	        while(j>0&&arr[j-1]>arr[j]){
+	            int temp=arr[j-1];
+	            arr[j-1]=arr[j];
+	            arr[j]=temp;
+	            j--;
+	        }
+	   	}
 		System.out.println(Arrays.toString(arr));
 	}
 }
